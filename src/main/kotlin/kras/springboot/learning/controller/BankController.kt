b8398b592d5d1ca4416306de8866804cef20d3b5
andrew-kras <andrewkrasj@mail.ru>
2024-04-24 15:52:47 +0300

    web layer + test for api/banks GET

---
+++ src/main/kotlin/kras/springboot/learning/controller/BankController.kt
@@ -0,0 +1,16 @@
+package kras.springboot.learning.controller
+
+import kras.springboot.learning.model.Bank
+import kras.springboot.learning.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping()
+    fun getBanks(): Collection<Bank> = service.getBanks()
+//    fun getBanks(): String = "works"
+}
