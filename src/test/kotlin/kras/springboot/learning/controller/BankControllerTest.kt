7c6eba33270ac5a69b27c34ccd7888a552ee3b5b
andrew-kras <andrewkrasj@mail.ru>
2024-04-25 14:20:51 +0300

    dirties context

--- src/test/kotlin/kras/springboot/learning/controller/BankControllerTest.kt
+++ src/test/kotlin/kras/springboot/learning/controller/BankControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.*
 import org.xmlunit.util.Mapper
 import javax.print.attribute.standard.Media
@@ -176,9 +177,10 @@ internal class BankControllerTest @Autowired constructor(
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class DeleteExistingBank {
         @Test
+        @DirtiesContext
         fun `should delete the bank with the given account number ` () {
             // given
-            val accountNumber = "2"
+            val accountNumber = "2" //"1234"
 
             // when
             mockMvc.delete(("$baseUrl/$accountNumber"))
